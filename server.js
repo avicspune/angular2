d4279e520d6171459c9d8134b2ed8d0d067149af
avicspune <39512004+avicspune@users.noreply.github.com>
2018-05-22 14:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+// server.js
 const express = require('express');
 const app = express();
-
-app.use(express.static(__dirname+'dist'));
-app.listen(process.env.PORT || 8080)
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
